dc4d3a767798c1547e81f85ae3b27324efd024af
Gabriel Alberto Lopez Olivo <NM37043@banvenez.corp>
2022-07-11 15:08:03 -0430

    cambios en footer

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ require('./app.js')
 let mainWindow = null
 
 function main() {
-  mainWindow = new BrowserWindow({ autoHideMenuBar: true })
+  mainWindow = new BrowserWindow({ autoHideMenuBar: true, backgroundColor: '#2E73C7' })
   mainWindow.maximize();
-  mainWindow.loadURL(`http://localhost:8080/`);
+  mainWindow.loadURL(`http://localhost:8080/`);  
   mainWindow.on('close', event => {
     mainWindow = null
   });
